b0b01efc5ede8471ee2fa2b2d103d9184057ba99
卢吉生 <2865911620@qq.com>
2023-12-11 18:03:01 +0800

    feat:配置登录、主页的路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,2 +1,33 @@
 import Vue from 'vue'
 import Router from 'vue-router';
+
+Vue.use(Router);
+
+export default new Router({
+    routes: [
+        {
+            path: '/login',
+            name: 'Login',
+            component: () => import('@/pages/login'),
+            meta: {
+                hideHeader: true,
+                trust: true,
+                noNeedLogin: true,
+            }
+        },
+        {
+            path: '/',
+            name: 'Layout',
+            component: () => import('@/pages/layout'),
+            redirect: { name: 'Home' },
+            children: [
+                {
+                    path: 'home',
+                    name: 'Home',
+                    component: () => import('@/pages/home/index'),
+                    redirect: { name: 'page-list' },
+                }
+            ]
+        }
+    ]
+})
