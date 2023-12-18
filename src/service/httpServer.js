dc1aa4e18b9e30aa503b546efffabf2044de4bf0
卢吉生 <2865911620@qq.com>
2023-12-18 15:21:43 +0800

    feat:配置方法

--- src/service/httpServer.js
+++ src/service/httpServer.js
@@ -19,4 +19,149 @@ axios.interceptors.request.use(config => {
     config.headers.Authorization = store.getters.authorization;
     // 设置请求头中的x-csrf-token值为从cookie中获取的x-csrf-token
     config.headers['x-csrf-token'] = Cookie.get('x-csrf-token');
+    return config;
+}, error => {
+    return Promise.reject(error);
 })
+
+//响应拦截器异常处理
+axios.interceptors.response.use(response => {
+    if (response.data.status) {
+        return Promise.resolve(response.data);
+    } else {
+        store.dispatch('showMessage', {
+            type: 'error',
+            data: response.data.message,
+        })
+        return Promise.reject(response);
+    }
+}, error => {
+    if (error && error.response) {
+        switch (err.response.status) {
+            case 400:
+                error.message = '错误请求';
+                break;
+            case 401:
+                error.message = '未授权，请重新登录';
+                break;
+            case 403:
+                error.message = '拒绝访问';
+                break;
+            case 404:
+                error.message = '请求错误,未找到该资源';
+                break;
+            case 405:
+                error.message = '请求方法未允许';
+                break;
+            case 408:
+                error.message = '请求超时';
+                break;
+            case 500:
+                error.message = '服务器端出错';
+                break;
+            case 501:
+                error.message = '网络未实现';
+                break;
+            case 502:
+                error.message = '网络错误';
+                break;
+            case 503:
+                error.message = '服务不可用';
+                break;
+            case 504:
+                error.message = '网络超时';
+                break;
+            default:
+                error.message = `连接错误${error.response.message}`;
+        }
+    } else {
+        error.message = "连接到服务器失败";
+    }
+    store.dispatch('showMessage', {
+        type: 'error',
+        data: error.message || error.response.message,
+    })
+    return Promise.reject(error.response);
+})
+
+/**
+ * 下载文件
+ */
+let downloadFile = url => {
+    window.open(url);
+}
+
+export default {
+    //get请求
+    get(url, param, responseType, header) {
+        return axios({
+            method: 'get',
+            url,
+            params: param || {},
+            headers: {
+                ...(header || {})
+            },
+            responseType: responseType,
+        })
+    },
+
+    //post请求
+    post(url, param, header) {
+        return axios({
+            method: 'post',
+            url,
+            data: param || {},
+            headers: {
+                ...(header || {}),
+                'Content-type': 'application/json;charset=UTF-8'
+            }
+        })
+    },
+    postFormData(url, param, header) {
+        return axios({
+            method: 'post',
+            url,
+            data: qs.stringify(param) || {},
+            headers: {
+                ...(header || {}),
+                'Content-type': 'application/x-www-form-urlencoded;charset=UTF-8'
+            }
+        })
+    },
+    put(url, param, header) {
+        return axios({
+            method: 'put',
+            url,
+            data: param || {},
+            headers: {
+                ...(header || {}),
+                'Content-type': 'application/json;charset=UTF-8'
+            }
+        })
+    },
+    delete(url, param, header) {
+        return axios({
+            method: 'delete',
+            url,
+            params: param || {},
+            headers: {
+                ...(header || {}),
+            }
+        })
+    },
+    //导出文件
+    getFile(url, param) {
+        let tempParam = {
+            ...(param || {})
+        }
+
+        //拼接地址
+        let list = [];
+        for (let key in tempParam) {
+            list.push(key.toString() + '=' + tempParam[key]);
+        }
+        url = $config.baseURL + url + '?' + list.join('&');
+        url = encodeURI(url);
+        downloadFile(url);
+    }
+}
