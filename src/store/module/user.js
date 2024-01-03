8e38fc12a61af789539d976c4cc5af90860d029f
卢吉生 <2865911620@qq.com>
2024-01-03 09:29:40 +0800

    feat:账户过期时间

--- src/store/module/user.js
+++ src/store/module/user.js
@@ -2,10 +2,12 @@
  * 用户相关
  */
 import { setLocalStorage, getLocalStorage } from '@/common/js/mUtils';
+// import {setSessionStorage,getSessionStorage} from "@/common/js/mUtils";
 const state = {
     access_token: '',
     permissionsList: [],
-    userInfo: {} // 用户信息
+    userInfo: {}, // 用户信息
+    // expiration_time: 0, // 过期时间
 }
 
 const actions = {};
@@ -37,6 +39,14 @@ const mutations = {
     },
 
     /**
+     * 更新时间/此为过期时间
+     */
+    // updateExpirationTime(state, data) {
+    //     state.expiration_time = data || 0;
+    //     mutations.saveUserToLocal(state);
+    // },
+
+    /**
      * 将 user state 数据存储在localstore里面
      */
     saveUserToLocal(state) {
@@ -53,6 +63,17 @@ const mutations = {
                 state[key] = user[key];
             }
         }
+    },
+
+    /**
+     * 退出后，个人信息清空
+     */
+    clearUserInfo(state) {
+        state.userInfo = {};
+        state.access_token = '';
+        state.permissionsList = [];
+        state.expiration_time = 0;
+        mutations.saveUserToLocal(state);
     }
 }
 
