f0bd930220204bc40edde0c82a5b214bbaf7d427
卢吉生 <2865911620@qq.com>
2024-01-03 09:30:35 +0800

    feat:重置密码页面的完善

--- src/pages/password/password.vue
+++ src/pages/password/password.vue
@@ -1,12 +1,26 @@
 <template>
-  <div>重置密码</div>
+  <div class="password-step">
+    <Step v-if="active === 0" @next="stepNext"></Step>
+  </div>
 </template>
 
 <script>
+import Step from "./step/step.vue";
 export default {
-
-}
+  components: {
+    Step,
+  },
+  data() {
+    return {
+      active: 0,
+    };
+  },
+  methods:{
+    stepNext(value){
+      this.active=value;
+    }
+  }
+};
 </script>
 <style lang="less" scoped>
-
 </style>
---
+++ src/pages/password/step/step.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="header-title">找回密码</div>
+    <div class="header-secondary-title">验证码将会发送至你注册的邮箱</div>
+    <div class="password-input">
+      <el-input
+        v-model="email"
+        placeholder="请输入邮箱"
+        prefix-icon="el-icon-message"
+      ></el-input>
+    </div>
+    <div class="password-button">
+      <el-button type="primary" @click="next">下一步</el-button>
+    </div>
+    <div class="password-footer">
+      <el-link type="primary">手机号找回</el-link>
+      <el-link type="info">手机或邮箱不可用?</el-link>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Step",
+  data() {
+    return {
+      email: "",
+      active: 0,
+    };
+  },
+  methods: {
+    next() {
+      this.$emit("next", this.active + 1);
+    },
+  },
+};
+</script>
+<style lang="less" scoped>
+</style>
