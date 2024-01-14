a84233658794610efc096b83f024bae409e809ca
mika <2865911620@qq.com>
2024-01-15 01:09:12 +0800

    feat: 日期过滤器

--- src/filters/index.js
+++ src/filters/index.js
@@ -1,8 +1,13 @@
 /**
  * 封装过滤器
  */
-const filter = {
+import dayjs from "dayjs";
 
+const filter = {
+    //日期格式化
+    dateFormat: function (value) {
+        return dayjs(value).format("YYYY-MM-DD HH:mm:ss");
+    }
 };
 
 export default filter;
