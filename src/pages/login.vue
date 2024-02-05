52c286d89caa6a8a8450b559c45d1c0b320f1a4d
yunque <3190458890@qq.com>
2024-02-06 00:42:34 +0800

    style: 测试

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -134,7 +134,6 @@ export default {
   },
   created() {
     this.getRsa();
-    console.log('测试');
     this.formUrl = this.$route.query.from
       ? window.decodeURIComponent(this.$route.query.from)
       : "";
